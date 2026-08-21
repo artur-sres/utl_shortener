d457096f1579667a28bcf0f4bceef4ff3668b256
Artur <asr.engsoft@gmail.com>
2026-08-20 23:42:58 -0300

    Initial commit

---
+++ src/main/java/com/urlshortener/UtlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtlShortenerApplication.class, args);
+	}
+
+}
